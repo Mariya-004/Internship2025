7ed2db6ea5d31d59c6099543eed2979cf8e06b7b
Mariya004 <mariyajyothy04@gmail.com>
2025-05-05 14:03:51 +0530

    modified NumberGuessing.py

--- WEEK_1/May_5/NumberGuessing.py
+++ WEEK_1/May_5/NumberGuessing.py
@@ -1,37 +1,30 @@
-import random
+import random 
 
 def number_guessing_game():
     print("******** Number Guessing Game ********")
-    print("Guess a random integer")
-    l=int(input("Enter the lower limit:"))
+    print("Guess a number")
+    l=int(input("Enter the lower limt:"))
     u=int(input("Enter the upper limit:"))
-    attempts = 0
+    attempt=0
     while True:
-        random_number = random.randint(l, u)
-        attempts += 1
-        print("Is it ", random_number, "?")
-        response=(input("y/n:"))
-        if response=="y":
-            print("The number is ", random_number)
-            print("Number guessed in ", attempts, " attempts")
+        n=random.randint(l,u)
+        attempt+=1
+        print("Is the number ",n,"?")
+        ans=input("Enter (y/n):")
+        if ans=="y":
+            print("The number is ",n," guessed in ",attempt," attempts")    
             break
-        elif response=="n":
-            mid=(int((l+u)/2))
-            print("Is it less than ", mid, "? (y/n):")
-            a=input()
-            if a=="y" or "Y":
-                u=mid
-                l=l
-                random_number = random.randint(l, u)
-            elif a=="n" or "N":
-                l=mid
-                u=u
-                random_number = random.randint(l, u)
+        if ans=="n":
+            print("Is the number greater than ",n,"?")
+            ans=input("Enter (y/n):")
+            if ans== "y":
+                l=n+1
+            elif ans=="n":
+                u=n-1
             else:
                 print("Invalid input!")
                 break
         else:
             print("Invalid input!")
-            break       
+            break
 number_guessing_game()
-            
