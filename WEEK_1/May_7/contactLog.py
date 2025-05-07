a06c65f40baa40be90ce9036be5a99c9b0b38987
Mariya004 <mariyajyothy04@gmail.com>
2025-05-07 12:53:52 +0530

    added comments to contactLog.py

--- WEEK_1/May_7/contactLog.py
+++ WEEK_1/May_7/contactLog.py
@@ -1,15 +1,15 @@
 def contactLog():
-    dic={}
-    f=open("contacts.txt","a+")
+    dic={} # creating a dictionary
+    f=open("contacts.txt","a+") #opening the file in append mode
     f.write("name contact\n")
     num=int(input("Enter the number of contacts:"))
     for i in range(num):
         key=input("Enter the name:")
         value=input("Enter the contact:")
-        dic[key]=value
+        dic[key]=value   #storing the key value pairs in a dictionary
     for name, contact in dic.items():
-        f.write(name+" : "+contact+"\n")
-    f.seek(0)
+        f.write(name+" : "+contact+"\n") #writing the key value pairs into the dictionary
+    f.seek(0) #moving the file pointer to the start of the file to read its contents
     print("File contents")
-    print(f.read())
+    print(f.read()) #printing the contents of the file
 contactLog()
