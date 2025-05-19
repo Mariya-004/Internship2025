d8f085f6f909e7c4fed986360e9cdff94a0de7e1
Mariya004 <mariyajyothy04@gmail.com>
2025-05-19 13:34:04 +0530

    ADDED files

--- WEEK_2/May_16/Autogen/app.py
+++ WEEK_2/May_16/Autogen/app.py
@@ -1,45 +1,37 @@
-import autogen
+import asyncio
+from autogen_agentchat.agents import AssistantAgent
+from autogen_agentchat.conditions import TextMentionTermination
+from autogen_agentchat.ui import Console  # If you want to use Console
+from autogen_ext.models.openai import OpenAIChatCompletionClient
+from autogen_agentchat.teams import RoundRobinGroupChat
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 
-config_list = [
-    {
-        'model': 'gemini-2.0-flash',
-        'api_key': os.getenv("api_key"),  # Use os.getenv and import os
-        'api_type':'google'
-    }
-]  
+async def main():
+    model_client = OpenAIChatCompletionClient(
+        model="gemini-1.5-flash-8b",
+        api_key=os.getenv("API_KEY")
+    )
 
-llm_config={
-    "seed": 42,
-    "config_list": config_list,
-    "temperature": 0,  #lower the temperature for less creative and less unique replies higher temperature for more creative and unique replies
-     
-}
+    assistant = AssistantAgent(
+        name="assistant",
+        model_client=model_client,
+    )
 
-assistant=autogen.AssistantAgent(
-    name="assistant",     #we can also  add more number of agents
-    llm_config=llm_config
-    #if adding more than one agent we have to define system message to specify the roles
-)
+    termination = TextMentionTermination("TERMINATE")
 
-user_proxy=autogen.UserProxyAgent(
-    name="user_proxy",
-    human_input_mode="TERMINATE",
-    max_consecutive_auto_reply=10,
-    is_termination_msg=lambda x: x.get("content", "").rstrip().endswith("TERMINATE"),
-    code_execution_config={"work_dir": ".", "use_docker": False},  # Use "." for current directory
-    llm_config=llm_config,
-    system_message="""Reply TERMINATE if the task has been solved at full satisfaction. Otherwise, reply CONTINUE or the reason why the task is not solved yet."""
-)
-                      #agent that acts on behalf of the user
+    # If Console supports single agent usage without GroupChat
+    group_chat = RoundRobinGroupChat([assistant],termination_condition=termination)
 
-task="""
-tell me a joke
-"""
 
-user_proxy.initiate_chat(
-    assistant,
-    message=task,
-)
+    # Run the conversation/task
+    await Console(group_chat.run_stream(task="Is the earth flat?"))
+
+    await model_client.close()
+
+if __name__ == "__main__":
+    asyncio.run(main())
