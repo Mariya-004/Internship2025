e4bdf946c5390dc68ff43ef35dc5ac8b78ec08b4
Mariya004 <mariyajyothy04@gmail.com>
2025-05-21 13:12:01 +0530

    Commit

--- WEEK_3/May_20/app.py
+++ WEEK_3/May_20/app.py
@@ -13,17 +13,44 @@ import os
 from dotenv import load_dotenv
 from concurrent.futures import ThreadPoolExecutor
 from pylint.lint import Run
+from pylint.reporters.text import TextReporter
+from io import StringIO
+
 
 
 load_dotenv()
 # Load environment variables from .env file
-async def main():
-    model_client = OpenAIChatCompletionClient (
-        model="gemini-1.5-flash-8b",
-        api_key=os.getenv("API_KEY") # Load from .env
-)
+def run_pylint(code:str)->str:
+    tmp_file="temp_code.py"
+    with open (tmp_file, "w") as f:
+        f.write(code)
+    output=StringIO()
+    reporter=TextReporter(output=output)
+    try:
+        Run([tmp_file],reporter=reporter,do_exit=False)
 
-#define the coder agent
+    except Exception as e:
+        return f"Pylint failed {e}"
+    return output.getvalue()
+
+def extract_code_from_message(message):
+    # Extract the code from the message
+    # Assuming the code is enclosed in triple backticks
+    if "```" in message:
+        start = message.index("```") + 3
+        end = message.rindex("```")
+        return message[start:end].strip()
+    return None
+
+
+
+async def main():
+    model_client = OpenAIChatCompletionClient(
+        model="gemini-1.5-flash-8b",
+        api_key=os.getenv("API_KEY")  # Load from .env
+    )
+
+    #define the coder agent
     coder = AssistantAgent(
         name="Coder",
         model_client=model_client,
@@ -36,18 +63,20 @@ async def main():
         name="Debugger",
         model_client=model_client,
         description="You are a helpful debugging assistant.",
-        system_message="You are a helpful debugging assistant when done debugging type Terminate.",
-        
+        system_message=(
+            "You're a Python debugger. Given Python code, use the function `run_pylint(code: str) -> str` "
+            "to find issues. Then, rewrite or suggest fixes. When the code is clean, type 'Terminate'."
+        )
     )
-    termination_condition=TextMentionTermination("Terminate")
-    # Create a group chat with round-robin communication
-    group_chat = RoundRobinGroupChat([coder, debugger],termination_condition=termination_condition)
 
-    # Create a console UI for the group chat
-    t=input("Enter the program to generate code for: ")
+    termination_condition = TextMentionTermination("Terminate")
+    group_chat = RoundRobinGroupChat([coder, debugger], termination_condition=termination_condition)
+
+    t = input("Enter the program to generate code for: ")
     await Console(group_chat.run_stream(task=t))
 
     await model_client.close()
+
 # Run the main function
 if __name__ == "__main__":
     asyncio.run(main())
